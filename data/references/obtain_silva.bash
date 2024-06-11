ea38d8df8d6d9f126309067ca1a21f8b4d217255
Prisca Lim <prisca@live.unc.edu>
2024-06-11 10:06:07 -0400

    Added command to cd into data directory

--- data/references/obtain_silva.bash
+++ data/references/obtain_silva.bash
@@ -2,6 +2,7 @@
 
 # This script provides instructions on obtaining reference files from the SILVA 138.1 release using the following lines:
 
+cd data/references
 wget https://ftp.arb-silva.de/release_138_1/Exports/taxonomy/tax_slv_ssu_138.1.txt.gz
 wget https://ftp.arb-silva.de/release_138_1/Exports/SILVA_138.1_SSURef_NR99_tax_silva.fasta.gz
 
