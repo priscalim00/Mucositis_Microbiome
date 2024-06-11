ca3d31b8daa94d55a8ff942152a69b4a8a8eaa09
Prisca Lim <prisca@live.unc.edu>
2024-06-11 10:13:30 -0400

    Created analysis driver bash script

---
+++ code/full_analysis.bash
@@ -0,0 +1,8 @@
+#/usr/bin/env bash
+
+# This bash script contains the complete analysis. Theoretically, running this script would complete the entire analysis
+# if no bugs are present. Currently still a work in progress
+
+# Obtain reference database from SILVA v138.1
+
+# Obtain raw sequence files from /proj/AndermannLab
